69283bc3d9fb7c619a12b889e7aecf4d1c948529
wilsonjhub <jasonstimphil@gmail.com>
2022-06-08 19:47:02 -0400

    uncommented the login GetMapping/PostMapping

--- src/main/java/com/codefellowship/CodeFellowShip/controller/CodeFellowshipController.java
+++ src/main/java/com/codefellowship/CodeFellowShip/controller/CodeFellowshipController.java
@@ -71,7 +71,7 @@ public class CodeFellowshipController {
 
 
 
-//    post to login
+////    post to login
     @PostMapping ("/login")
     public RedirectView logInUser(String username, String password){
 
