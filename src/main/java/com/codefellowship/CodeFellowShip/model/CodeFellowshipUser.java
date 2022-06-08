2c49f39faf7af8d6dbba588dcc81ecb03c12ae6e
wilsonjhub <jasonstimphil@gmail.com>
2022-06-07 23:19:06 -0400

    decided to add a few other properties to the model. added the corresponding props to controller. commented out the Get/PostMapping for /login for now. Will add at a later time

--- src/main/java/com/codefellowship/CodeFellowShip/model/CodeFellowshipUser.java
+++ src/main/java/com/codefellowship/CodeFellowShip/model/CodeFellowshipUser.java
@@ -12,12 +12,28 @@ public class CodeFellowshipUser {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Long id;
     private String username;
     private String password;
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private String bio;
 
-
-    public CodeFellowshipUser(String username, String password) {
+    public CodeFellowshipUser(String username, String password, String firstName, String lastName, String dateOfBirth, String bio) {
         this.username = username;
         this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.bio = bio;
     }
+
+//    public CodeFellowshipUser(String username, String password) {
+//        this.username = username;
+//        this.password = password;
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//        this.dateOfBirth = dateOfBirth;
+//        this.bio = bio;
+//    }
 
     public String getUsername() {
         return username;
@@ -33,5 +49,37 @@ public class CodeFellowshipUser {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
     }
 }
